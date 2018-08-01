0242ccf350f88053593cb9a14b52020596050396
Kyle Hennig <henn.kyle@outlook.com>
2018-08-01 12:01:23 -0600

    Use pygame instead of OpenCV

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 
-import cv2
+import pygame
+import pygame.camera
+import pygame.image
 from PIL import Image
 
 from stream_handler import StreamHandler
@@ -9,16 +11,19 @@ from stream_handler import StreamHandler
 class WebcamStreamHandler(StreamHandler):
     def __init__(self, application, request):
         try:
-            self.camera = cv2.VideoCapture(0)
+            pygame.camera.init()
+            cameras = pygame.camera.list_cameras()
+            self.camera = pygame.camera.Camera(cameras[0])
+            self.camera.start()
         except Exception as e:
             print("Could not connect to the webcam.")
             print(e)
         super().__init__(application, request)
 
     def get_frame(self):
-        _, img = self.camera.read()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(img)
+        img = self.camera.get_image()
+        img = pygame.image.tostring(img, "RGBA", False)
+        img = Image.frombytes("RGBA", img)
         # Saves the image to a bytes buffer.
         buffer = BytesIO()
         img.save(buffer, format="JPEG")
